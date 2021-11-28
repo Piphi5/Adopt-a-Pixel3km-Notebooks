e7860507aff613fdef3615ef89fa553e61115f53
Matteo <mateus.sakata@gmail.com>
2021-11-27 22:33:41 -0600

    Add csv combining utility

---
+++ utils/code/combine_csvs.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import os
+
+
+def get_csvs(directory):
+    df = pd.DataFrame()
+    csvs = [file for file in os.listdir(directory) if file.endswith(".csv")]
+    for csv in csvs:
+        temp = pd.read_csv(f"{directory}/{csv}")
+        df = df.append(temp)
+
+    return df
